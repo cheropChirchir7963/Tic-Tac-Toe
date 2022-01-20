b495e8faafab71c5321b38c3524a52c82d921bdb
cheropChirchir7963 <cheropmarlene@gmail.com>
2022-01-20 13:05:29 +0300

    add check if position valid

--- lib/square.ex
+++ lib/square.ex
@@ -3,17 +3,18 @@ defmodule TicTacToe.Square do
 
   @board_size 3
   @enforce_keys [:pos]
-
+  @max_pos @board_size * @board_size
 
   defstruct [:x, :y, :val, :pos ]
 
-  def new(pos) do
+  def new(pos) when pos > 0 and pos <= @max_pos do
     %TicTacToe.Square{pos: pos, y: y(pos), x: x(pos)}
   end
 
-  #helper functions for x and y
+  def x(%Square{pos: pos}), do: x(pos)
   def x(pos), do: rem(pos - 1, @board_size) + 1
-  def y(pos), do: div(pos - 1, @board_size) + 1
 
+  def y(%Square{pos: pos}), do: y(pos)
+  def y(pos), do: div(pos - 1, @board_size) + 1
 
 end
